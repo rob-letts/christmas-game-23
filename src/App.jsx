666dee41aa75ccb13836b5f1e481ce9f46f862df
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2023-12-16 20:34:36 +0000

    initial logic and styles for checks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
-// import { createSignal } from 'solid-js';
-// import './App.css';
+import { For, createSignal } from 'solid-js';
+import './App.css';
 
 function App () {
-  // const [count, setCount] = createSignal(0);
+  const [isNoughtsTurn, setIsNoughtsTurn] = createSignal(true);
+  const [checks, setChecks] = createSignal(Array.from({ length: 9 }, () => ``));
+
+  function change (index) {
+    setChecks(checks().map((check, i) =>
+      i === index
+        ? isNoughtsTurn()
+          ? `nought`
+          : `cross`
+        : check
+    ));
+    setIsNoughtsTurn(!isNoughtsTurn());
+  }
 
   return (
     <>
-      <h1>Merry Christmas River</h1>
+      <header>
+        <h1 class="title">Merry Christmas River</h1>
+      </header>
+      <fieldset>
+        <legend class="turn-info">
+          {isNoughtsTurn() ? `Noughts` : `Crosses`} turn
+        </legend>
+        <div class="checkbox-grid">
+          <For each={checks()}>
+            {(check, i) =>
+              <>
+                <input
+                  type="checkbox"
+                  id={i()}
+                  onClick={() => change(i())}
+                />
+                <label
+                  class={checks()[i()]}
+                  for={i()}
+                />
+              </>
+            }
+          </For>
+        </div>
+      </fieldset>
     </>
   );
 }
