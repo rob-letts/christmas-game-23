dd36dad88c8dd295106125331b7aff4fbc0271ed
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2023-12-17 13:46:02 +0000

    format config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solid()],
-  server: {
-    port: 3000
-  }
-})
+  server: { port: 3000 }
+});
